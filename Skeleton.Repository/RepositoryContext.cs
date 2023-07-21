01ee8f509b8c5a6a7661c072e844fd0838019d18
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-07-21 21:46:38 +0200

    feat: create repository context factory

---
+++ Skeleton.Repository/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skeleton.Entities.Models;
+
+namespace Skeleton.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Company>? Companies { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+}
---
+++ Skeleton/Factory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Skeleton.Repository;
+
+namespace Skeleton.Factory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    private const string APP_SETTINGS = "appsettings.json";
+
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(APP_SETTINGS)
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
