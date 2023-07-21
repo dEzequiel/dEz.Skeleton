141aa84f4e0791404ca68024d97445f78c030692
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-07-21 21:42:15 +0200

    feat: skeleton business entities

---
+++ Skeleton.Entities/Models/Company.cs
@@ -0,0 +1,32 @@
+﻿namespace Skeleton.Entities.Models;
+
+/// <summary>
+/// Company business entity.
+/// </summary>
+public class Company
+{
+    /// <summary>
+    /// Company identifier.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Company name.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Company address.
+    /// </summary>
+    public string? Address { get; set; }
+
+    /// <summary>
+    /// Company country.
+    /// </summary>
+    public string? Country { get; set; }
+
+    /// <summary>
+    /// Employees attached to Company.
+    /// </summary>
+    public ICollection<Employee>? Employees { get; set; }
+}
---
+++ Skeleton.Entities/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿namespace Skeleton.Entities.Models;
+
+/// <summary>
+/// Employee business entity.
+/// </summary>
+public class Employee
+{
+    /// <summary>
+    /// Employee Identifier.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Employee name.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Employee age.
+    /// </summary>
+    public int Age { get; set; }
+
+    /// <summary>
+    /// Employee position.
+    /// </summary>
+    public string? Position { get; set; }
+
+    /// <summary>
+    /// Attached company identifier.
+    /// </summary>
+    public Guid CompanyId { get; set; }
+
+    /// <summary>
+    /// Company attached to Employee.
+    /// </summary>
+    public Company? Company { get; set; }
+}
