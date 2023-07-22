7c8a8175e5df198f8cb4b9a733d5f663cd11daa5
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-07-22 22:19:41 +0200

    feat: ef core entities model configuration

--- Skeleton.Repository/RepositoryContext.cs
+++ Skeleton.Repository/RepositoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Skeleton.Entities.Models;
+using Skeleton.Repository.Configuration;
 
 namespace Skeleton.Repository;
 
@@ -7,6 +8,12 @@ public class RepositoryContext : DbContext
 {
     public RepositoryContext(DbContextOptions options) : base(options)
     {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
     }
 
     public DbSet<Company>? Companies { get; set; }
