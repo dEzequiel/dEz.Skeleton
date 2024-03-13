4eb3d1fbc164ca5e07880bab22cec5688abbedd2
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-14 00:33:38 +0100

    feat: define and implement command dispatcher for registering handlers and send to handler method

---
+++ Skeleton.CQRSCore/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using Skeleton.CQRSCore.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.CQRSCore.Infrastructure
+{
+    public interface ICommandDispatcher
+    {
+        /// <summary>
+        /// RegisterHandler allows to register a command handler method.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="handler"></param>
+        void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+        
+        /// <summary>
+        /// SendAsync dispatch the command object to the registered command handler method.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        Task SendAsync(BaseCommand command);
+    }
+}
---
+++ Skeleton.Presentation/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,41 @@
+﻿using Skeleton.CQRSCore.Commands;
+using Skeleton.CQRSCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Presentation.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        /// <summary>
+        /// Registered handlers.
+        /// </summary>
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
+
+        /// <inheritdoc cref="ICommandDispatcher"/>
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if(_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("You cannot register the same command handler twice");
+            }
+
+            _handlers.Add(typeof(T), x => handler((T)x));
+        }
+
+        /// <inheritdoc cref="ICommandDispatcher"/>
+        public async Task SendAsync(BaseCommand command)
+        {
+            if(_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+            {
+                await handler(command);
+            } else
+            {
+                throw new ArgumentNullException(nameof(handler), "No command handler was registered");
+            }
+        }
+    }
+}
