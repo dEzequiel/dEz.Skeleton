5f3b41d0c46bb0b50ef397bd105c6180270ae5c6
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-14 17:37:46 +0100

    feat; event handler for company aggregates implementation

---
+++ Skeleton.CQRSCore/Infrastructure/IEventSourcingHandler.cs
@@ -0,0 +1,33 @@
+﻿using Skeleton.CQRSCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.CQRSCore.Infrastructure
+{
+    /// <summary>
+    /// Interface defining operations for saving and retrieving aggregates and republishing events.
+    /// </summary>
+    public interface IEventSourcingHandler<T>
+    {
+        /// <summary>
+        /// Saves the provided aggregate.
+        /// </summary>
+        /// <param name="aggregate">The aggregate to save.</param>
+        Task SaveAsync(AggregateRoot aggregate);
+
+        /// <summary>
+        /// Retrieves an aggregate by its unique identifier.
+        /// </summary>
+        /// <param name="aggregateId">The unique identifier of the aggregate.</param>
+        /// <returns>The retrieved aggregate.</returns>
+        Task<T> GetByIdAsync(Guid aggregateId);
+
+        /// <summary>
+        /// Republishes events.
+        /// </summary>
+        Task RepublishEventsAsync();
+    }
+}
---
+++ Skeleton.Presentation/Handlers/CompanyEventSourcingHandler.cs
@@ -0,0 +1,44 @@
+﻿using Skeleton.CQRSCore.Domain;
+using Skeleton.CQRSCore.Infrastructure;
+using Skeleton.Entities.Aggregates;
+using Skeleton.Service.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Presentation.Handlers
+{
+    public class CompanyEventSourcingHandler : IEventSourcingHandler<CompanyAggregate>
+    {
+        private readonly IEventStore _eventStore;
+
+        /// <inheritdoc cref="IEventSourcingHandler{T}"/>
+        public async Task<CompanyAggregate> GetByIdAsync(Guid aggregateId)
+        {
+            var aggregate = new CompanyAggregate();
+            var events = await _eventStore.GetEventsAsync(aggregateId);
+
+            if (events is null || !events.Any()) return aggregate;
+
+            aggregate.ReplayEvents(events);
+            aggregate.Version = events.Select(x => x.Version).Max();
+
+            return aggregate;
+        }
+
+        /// <inheritdoc cref="IEventSourcingHandler{T}"/>
+        public Task RepublishEventsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc cref="IEventSourcingHandler{T}"/>
+        public async Task SaveAsync(AggregateRoot aggregate)
+        {
+            await _eventStore.SaveEventAsync(aggregate.Id, aggregate.GetType().Name, aggregate.GetUncommitedChanges(), aggregate.Version);
+            aggregate.MarkChangesAsCommited();
+        }
+    }
+}
