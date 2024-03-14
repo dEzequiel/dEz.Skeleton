59b96498d0f43cd79a36c059d0f4ca2e5ecda7c8
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-14 16:34:41 +0100

    feat: define EventModel

---
+++ Skeleton.CQRSCore/Events/EventModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Skeleton.CQRSCore.Events
+{
+    public class EventModel 
+    {
+        /// <summary>
+        /// Event stored identifier.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Time stamp when the event is persisted.
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+
+        /// <summary>
+        /// Aggregate identifier that the events relates to.
+        /// </summary>
+        public Guid AggregateIdentifier { get; set; }
+
+        /// <summary>
+        /// Aggregate type.
+        /// </summary>
+        public string AggregateType { get; set; }
+
+        /// <summary>
+        /// Version of the aggregate at this point in time.
+        /// </summary>
+        public int Version { get; set; }
+
+        /// <summary>
+        /// Event type.
+        /// </summary>
+        public string EventType { get; set; }
+
+        /// <summary>
+        /// Polymorphism allow us to assign an instance of a concrete event object to the base event.
+        /// That will ensure that we have all of the event information.
+        /// 
+        /// Useful for replaying the event store.
+        /// </summary>
+        public BaseEvent EventData { get; set; }
+    }
+}
