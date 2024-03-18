1f59ad96949bef4f0a287626bb2f4411d56cea94
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-18 15:10:24 +0100

    fix

---
+++ Skeleton.CQRSCore/Domain/IEventStoreRepository.cs
@@ -0,0 +1,27 @@
+﻿using Skeleton.CQRSCore.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.CQRSCore.Domain
+{
+    public interface IEventStoreRepository
+    {
+        /// <summary>
+        /// Store an event.
+        /// </summary>
+        /// <param name="event"></param>
+        /// <returns></returns>
+        void SaveAsync(EventModel @event);
+
+        /// <summary>
+        /// Asynchronously retrieves all events related to a aggregate.
+        /// </summary>
+        /// <param name="aggregateId">The unique identifier of the aggregate to retrieve events.</param>
+        /// <returns>A task representing the asynchronous operation, returning an enumerable collection of
+        /// <see cref="EventModel"/>.</returns>
+        Task<IEnumerable<EventModel>> FindByAggregateId(Guid aggregateId);
+    }
+}
